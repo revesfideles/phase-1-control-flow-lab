c8b54516639ae19c11e75653d61487363f1b8663
revesfideles <revesfideles@gmail.com>
2024-02-17 02:40:51 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if(someValue <= 2000) {
+    return "That will be twenty bucks.";
+  } else if(someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let Generous;
+
+  switch (tip) {
+    case 'generous':
+      Generous = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      Generous = 'Thank you.';
+      break;
+    default:
+      Generous = 'Bye.'
+      break;
+  }
+  return Generous;
 }
